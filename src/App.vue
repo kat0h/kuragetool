ef22cd1155f3d90f02be7d03cd043f176e4bdfd7
KotaKato <peony.btn@gmail.com>
2021-02-23 21:26:04 +0900

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,53 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+
+    <!-- ドロワー -->
+    <v-navigation-drawer
+    app v-model="drawer" temporary>
+      <v-list-item>
+        <v-list-item-title class="title">
+          🐦はとツール
+        </v-list-item-title>
+      </v-list-item>
+      <v-divider />
+      <v-list>
+        <!-- アプリ一覧 -->
+        <v-list-item link>
+          <v-list-item-icon>
+            <v-icon>mdi-apps</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            アプリ一覧
+          </v-list-item-content>
+        </v-list-item>
+        <!-- このサイトについて -->
+        <v-list-item link>
+          <v-list-item-icon>
+            <v-icon>mdi-information</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            このサイトについて
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <!-- 上部のバー -->
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+    </v-app-bar>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+  },
+
+  data: () => ({
+    drawer: false,
+  }),
+};
+</script>
