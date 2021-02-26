164d8f2f04ed3206c6beceffc6d3e462de81e5df
KotaKato <peony.btn@gmail.com>
2021-02-26 23:42:35 +0900

    Use Vue router

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
     <h1>このサイトについて</h1>
+    <ol>
+      <li v-for="l in d" :key="l.title">
+        {{ l.title }}
+      </li>
+    </ol>
   </div>
 </template>
 
@@ -8,6 +13,10 @@
 export default {
   name: 'AboutPage',
   data: () => ({
+    d: [
+    {title: "unko", ind: "u"},
+    {title: "unko2", ind: "i"},
+    ]
   }),
 };
 </script>
