20daa9f4fdfe1ee832623001ff52478068360a83
KotaKato <peony.btn@gmail.com>
2021-03-05 21:35:41 +0900

    New App word count

--- src/components/apps/wordcount.vue
+++ src/components/apps/wordcount.vue
@@ -1,15 +1,19 @@
 <template>
   <div style="text-align: center">
     <h1>
-    文字数カウント
+    文字数カウンター
     </h1>
     <v-container >
       <v-textarea
       filled
       label="計測する文章"
       auto-grow
-      :value="text"
+      v-model="text"
       ></v-textarea>
+      <div>
+        文字数：{{text.length}}<br>
+        原稿用紙：{{Math.floor(text.length/400)+1}}
+      </div>
     </v-container>
   </div>
 </template>
