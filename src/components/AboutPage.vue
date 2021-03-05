66144225791f8c32ade0e06b6ed8f1f505c8f7b7
KotaKato <peony.btn@gmail.com>
2021-03-06 00:31:52 +0900

    Update about page

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -1,13 +1,15 @@
 <template>
   <v-container>
   <div>
-    <h1>このサイトについて</h1>
+    <h3>このサイトについて</h3>
     <a href="https://twitter.com/uvrub">私(@uvrub)</a>が個人的に作ったブラウザ上でうごくツール達です。<br>
     処理は全てブラウザ上で処理されるため、データは一切保存されません。<br>
     Vue.jsとVuetifyで構築されており、ソースコードは<a href="https://github.com/kato-k/kuragetool">Github</a>上で公開されています。<br>
   </div>
   <div>
-    
+    <h3>リンク・連絡先 </h3>
+    <a href="https://twitter.com/uvrub">Twitter (@uvrub)</a><br>
+    <a href="https://github.com/kato-k">Github (@kato-k)</a><br>
   </div>
   </v-container>
 </template>
