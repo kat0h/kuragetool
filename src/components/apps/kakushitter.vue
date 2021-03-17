26c776dfc379c723e01e31d60c82d33e533654c8
KotaKato <peony.btn@gmail.com>
2021-03-17 23:10:59 +0900

    Fix typo and Add explanation

--- src/components/apps/kakushitter.vue
+++ src/components/apps/kakushitter.vue
@@ -1,9 +1,9 @@
 <template>
-    <div style="text-align: center">
-        <h1>
-        隠しtter
-        </h1>
-        <v-container>
+    <div>
+        <v-container style="text-align: center">
+            <h1>
+                隠しtter
+            </h1>
             <v-textarea
             filled
             label="隠す文章を入力"
@@ -12,9 +12,25 @@
             ></v-textarea>
             <h3>↓変換後↓</h3>
             <a :href="makeurl()" target="_blank">
-                文章を確かめる(新しいタブを開く)
+                文章を確める(新しいタブを開く)
             </a><br><br>
             <v-btn block v-on:click="copybtn">コピーする</v-btn>
+        </v-container>
+        <v-container>
+            <h3>
+                隠しtterとは？
+            </h3>
+            <div>
+                ふせったーのように好きな文章をネタバレを防ぎながら共有できるサービスです。<br>
+                文章を一目でわからない形式に、ブラウザ上で変換するため、データがサーバーに送られることは一切ありません。<br>
+            </div>
+            <h3>
+                使い方
+            </h3>
+            <div>
+                隠したい文章を入力し、コピーボタンを押すことでTwitterに貼り付けられるURLをコピーできます。<br>
+                コピーされたリンクをTwitterに貼り付けることで文章を共有できます。<br>
+            </div>
         </v-container>
     </div>
 </template>
